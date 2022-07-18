fe8e52988a21a5be0050d3f957fa9630afd17f32
hany2 <hany.alom@gmail.com>
2022-07-18 14:38:30 +0200

    Validation

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from main import app
 
-#Checks if the run.py file has executed directly and not imported
+# Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
